ddfb7d17f4a0c0253f5908edce989435a64e10fe
mulori <murilogarcia.hr@gmail.com>
2022-05-17 16:31:47 -0300

    liberando a primeira versão

---
+++ GuiaMedidasMLB/View/frmMensagemErro.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace GuiaMedidasMLB.View
+{
+    public partial class frmMensagemErro : Form
+    {
+        public frmMensagemErro(string Texto)
+        {
+            InitializeComponent();
+            txtTexto.Text = Texto;
+        }
+    }
+}
